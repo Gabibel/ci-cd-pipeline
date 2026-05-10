1e72d7ea4a52b9c7da71401d6b8fa7250436e7de
Gabriel <babibel1506@gmail.com>
2026-05-10 22:21:45 +0200

    t

---
+++ ci-cd-pipeline/backend/eslint.config.js
@@ -0,0 +1,20 @@
+const js = require('@eslint/js');
+
+module.exports = [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'commonjs',
+      globals: {
+        require: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
+        __dirname: 'readonly',
+        exports: 'readonly',
+      },
+    },
+    rules: {},
+  },
+];
